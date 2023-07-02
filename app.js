9d72aa899bb637e28ae54035d5c736851fe2cee7
Luis Carlos <luicarlosferreira@outlook.com>
2023-07-02 19:32:16 +0100

    changes

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const cors = require("cors");
 const UserRoute = require("./src/routes/userRoutes");
 const ProductRoute = require("./src/routes/productsRoutes");
 const PostRoute = require("./src/routes/postsRoutes");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT | 8000;
 
-app.use(express.json({ limit: "10mb", extended: true }));
+app.use(express.json());
+app.use(bodyParser.json({ limit: "100mb" }));
 app.use(
-  express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    limit: "100mb",
+    extended: true,
+  })
 );
 app.use(cors());
 
