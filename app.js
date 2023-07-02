05f85b02a046201c66723c8cf514477c301d1e45
Luis Carlos <luicarlosferreira@outlook.com>
2023-07-02 13:03:26 +0100

    changes

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const ProductRoute = require("./src/routes/productsRoutes");
 const PostRoute = require("./src/routes/postsRoutes");
 
 const PORT = process.env.PORT | 8000;
-app.use(express.json());
+
+app.use(express.json({ limit: "10mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
+);
 app.use(cors());
-app.use(express.bodyParser({ limit: "50mb" }));
 
 // app.use(
 //     cors({
