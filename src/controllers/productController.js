3842a4a97c47ebf13ba3722abebb391305c3d6f1
Luis Carlos <luicarlosferreira@outlook.com>
2023-07-02 20:23:14 +0100

    changes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -12,9 +12,12 @@ const CreateProductController = async (req, res) => {
   const { category, title, price, brand, image } = req.body;
 
   try {
-    const resultImage = await cloudinary.uploader.upload(image, {
-      folder: "products",
-    });
+    const resultImage = await cloudinary.uploader.upload(
+      { image },
+      {
+        folder: "products",
+      }
+    );
 
     const data = await CreateProduct({
       category,
