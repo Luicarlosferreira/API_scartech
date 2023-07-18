c9db8fd2a4aa79b360d6c162b1638b7385fb883b
Luis Carlos <luicarlosferreira@outlook.com>
2023-07-18 14:37:33 +0100

    changes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const { MongoClient } = require("mongodb");
 const express = require("express");
 const app = express();
 require("dotenv").config();
@@ -8,8 +7,6 @@ const ProductRoute = require("./src/routes/productsRoutes");
 const PostRoute = require("./src/routes/postsRoutes");
 
 const PORT = process.env.PORT | 8000;
-const uri = process.env.MONGO_CONNECTION_STRING;
-const client = new MongoClient(uri);
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb" }));
@@ -23,13 +20,4 @@ app.use(cors());
 //   );
 app.use("/", UserRoute, ProductRoute, PostRoute);
 
-client.connect((err) => {
-  if (err) {
-    console.error(err);
-    return false;
-  }
-  // connection to mongo is successful, listen for requests
-  app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-  });
-});
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
